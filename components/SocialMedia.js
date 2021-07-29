3ce1a76d619ef36646fee330311bac108935a55e
GabrielNumaX <g.numa10@gmail.com>
2021-07-29 07:08:45 -0300

    fontawesome Icon BUG FIX-> check SocialMedia.js

--- components/SocialMedia.js
+++ components/SocialMedia.js
@@ -1,6 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedinIn, faGithub, faFacebookF, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
+
 
 const SocialMedia = () => {
     return (
@@ -39,6 +46,85 @@ const SocialMedia = () => {
                         <a href="https://www.linkedin.com/in/numax" target="_blank" rel="noopener noreferrer">
 
                             <FontAwesomeIcon icon={faLinkedinIn} className="icon" />
+
+                        </a>
+
+                    </div>
+                </div>
+
+                <div className="socialCard">
+
+                    <div className="socialText">
+
+                        <h3>Github</h3>
+                        <p>Professional profile</p>
+
+                    </div>
+
+                    <div className="socialIcon">
+
+                        <a href="https://github.com/GabrielNumaX" target="_blank" rel="noopener noreferrer">
+
+                            <FontAwesomeIcon icon={faGithub} className="icon" />
+
+                        </a>
+
+                    </div>
+                </div>
+
+                <div className="socialCard">
+
+                    <div className="socialText">
+
+                        <h3>Facebook</h3>
+                        <p>Professional profile</p>
+
+                    </div>
+
+                    <div className="socialIcon">
+
+                        <a href="https://www.facebook.com/gabriel.numax" target="_blank" rel="noopener noreferrer">
+
+                            <FontAwesomeIcon icon={faFacebookF} className="icon" />
+
+                        </a>
+
+                    </div>
+                </div>
+
+                <div className="socialCard">
+
+                    <div className="socialText">
+
+                        <h3>Instagram</h3>
+                        <p>Social profile</p>
+
+                    </div>
+
+                    <div className="socialIcon">
+
+                        <a href="https://www.instagram.com/_.numax._" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faInstagram} className="icon" />
+
+                        </a>
+
+                    </div>
+                </div>
+
+                <div className="socialCard">
+
+                    <div className="socialText">
+
+                        <h3>Twitter</h3>
+                        <p>Social profile</p>
+
+                    </div>
+
+                    <div className="socialIcon">
+
+                        <a href="https://twitter.com/gabrielnumax" target="_blank" rel="noopener noreferrer">
+
+                            <FontAwesomeIcon icon={faTwitter} className="icon" />
 
                         </a>
 
