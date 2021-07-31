4d10fde8bc567c2b95b2ceb86d879c0ca3be6574
GabrielNumaX <37051017+GabrielNumaX@users.noreply.github>
2021-07-30 22:02:01 -0300

    profile img CSS FIX

--- components/Landing.js
+++ components/Landing.js
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Layout from './Layout';
+// import Layout from './Layout';
 
 const Landing = () => {
 
--- pages/_app.js
+++ pages/_app.js
@@ -31,7 +31,7 @@ function MyApp({ Component, pageProps }) {
 
         {/* fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;500;600&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
